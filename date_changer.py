339fcb96d1b7e4cefd2490e7f6c8abc5d1399656
lomonosov0 <shipa22rus@bk.ru>
2025-01-15 13:07:48 +0700

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,12 @@
+
+username  = "julia"
+title = "homework"
+content = "today"
+status = "ready"
+created_date = "13.01.2025"
+issue_date = "13.02.2025"
+
+print(created_date[0:5])
+print(issue_date[0:5])
+
+
