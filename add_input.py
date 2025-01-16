366c7856f750a4999e6d135c363eb932477dbc68
lomonosov0 <shipa22rus@bk.ru>
2025-01-16 18:43:30 +0700

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,14 @@
+
+username  = input("Введите имя пользователя: ")
+title = input("Введите заголовок заметки: ")
+content = input("Описание заметки: ")
+status = input("На каком этапе ваша заметка?: ")
+created_date = str(input("Дата создания заметки: "))
+issue_date = str(input("Дата истечения заметки: "))
+
+print(f"Имя: {username}\nЗаголовок: {title}\nОписание заметки: {content}\nСтатус: {status}\nДата создания: {created_date}\nДата истечения: {issue_date}\n")
+# print(username, title, content, status, created_date, issue_date)
+# print(created_date[0:5])
+# print(issue_date[0:5])
+
+
